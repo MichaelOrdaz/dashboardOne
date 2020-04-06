ac199be5887d4a70e3b4e1aa53e431a1864267c5
Michael Ordaz Martinez <emilydbz_11@hotmail.com>
2020-04-06 17:25:00 -0500

    feat(backend) Cambio estructura de una migracion

--- application/migrations/20200318132200_instancias.php
+++ application/migrations/20200318132200_instancias.php
@@ -40,7 +40,10 @@ class Migration_instancias extends CI_Migration {
         'descripcion'=> [
           'type'=> 'TEXT',
         ],
-
+        'public_ip'=> [
+          'type'=> 'varchar',
+          'constraint'=> '30',
+        ],
         'created_at datetime default current_timestamp',
         'updated_at datetime default current_timestamp on update current_timestamp',
         'status' => 
@@ -57,90 +60,98 @@ class Migration_instancias extends CI_Migration {
     //ingresar las instancias que ya existen y entan funcionando
     $data = [
       [
-        'nombre' => 'Instancia 100 LS+1 (192.168.0.100)',
+        'nombre' => 'Instancia 100 (192.168.0.100)',
         'host' => '192.168.0.100',
         'user' => 'application',
         'password' => 'Application123',
         'database' => 'Legal',
         'descripcion' => '',
+        'public_ip'=> '201.168.151.77:9010'
       ],
       [
-        'nombre' => 'Instancia 101 LS+1 (192.168.0.101)',
+        'nombre' => 'Instancia 101 (192.168.0.101)',
         'host' => '192.168.0.101',
         'user' => 'application',
         'password' => 'Application123',
         'database' => 'Legal',
         'descripcion' => '',
+        'public_ip'=> '201.168.151.77:8090'
       ],
       [
-        'nombre' => 'Instancia 102 LS+1 (192.168.0.102)',
+        'nombre' => 'Instancia 102 (192.168.0.102)',
         'host' => '192.168.0.102',
         'user' => 'application',
         'password' => 'Application123',
         'database' => 'Legal',
         'descripcion' => '',
+        'public_ip'=> '201.168.151.77:9030'
       ],
       [
-        'nombre' => 'Instancia 103 LS+1 (192.168.0.103)',
+        'nombre' => 'Instancia 103 (192.168.0.103)',
         'host' => '192.168.0.103',
         'user' => 'application',
         'password' => 'Application123',
         'database' => 'Legal',
         'descripcion' => '',
+        'public_ip'=> '201.168.151.77:8080'
       ],
       [
-        'nombre' => 'Instancia 104 LS+1 (192.168.0.104)',
+        'nombre' => 'Instancia 104 (192.168.0.104)',
         'host' => '192.168.0.104',
         'user' => 'application',
         'password' => 'Application123',
         'database' => 'Legal',
         'descripcion' => '',
+        'public_ip'=> '201.168.151.77:9090'
       ],
       [
-        'nombre' => 'Instancia 105 LS+1 (192.168.0.105)',
+        'nombre' => 'Instancia 105 (192.168.0.105)',
         'host' => '192.168.0.105',
         'user' => 'application',
         'password' => 'Application123',
         'database' => 'Legal',
         'descripcion' => '',
+        'public_ip'=> '201.168.151.77:9040'
       ],
       [
-        'nombre' => 'Instancia 106 LS+1 (192.168.0.106)',
+        'nombre' => 'Instancia 106 (192.168.0.106)',
         'host' => '192.168.0.106',
         'user' => 'application',
         'password' => 'Application123',
         'database' => 'Legal',
         'descripcion' => '',
+        'public_ip'=> '201.168.151.77:9050'
       ],
       [
-        'nombre' => 'Instancia 249 LS+1 (192.168.0.249)',
+        'nombre' => 'Instancia 249 (192.168.0.249)',
         'host' => '192.168.0.249',
         'user' => 'application',
         'password' => 'Application123',
         'database' => 'Legal',
         'descripcion' => '',
+        'public_ip'=> '201.168.151.77:8050'
       ],
       [
-        'nombre' => 'Instancia 250 LS+1 (192.168.0.250)',
+        'nombre' => 'Instancia 250 (192.168.0.250)',
         'host' => '192.168.0.250',
         'user' => 'application',
         'password' => 'Application123',
         'database' => 'Legal',
         'descripcion' => '',
+        'public_ip'=> '201.168.151.77:8070'
       ],
       [
-        'nombre' => 'Instancia 251 LS+1 (192.168.0.251)',
+        'nombre' => 'Instancia 251 (192.168.0.251)',
         'host' => '192.168.0.251',
         'user' => 'application',
         'password' => 'Application123',
         'database' => 'Legal',
         'descripcion' => '',
+        'public_ip'=> '201.168.151.77:8060'
       ],
     ];
 
     $this->db->insert_batch('instancias', $data );
-
-
 
   }
 
